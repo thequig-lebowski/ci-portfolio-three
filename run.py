e5cd74f3e19df95963938e77aa12bc0241155717
David O'Quigley <oquigld2@gmail.com>
2024-09-22 21:48:12 +0000

    Add input validation

--- run.py
+++ run.py
@@ -4,6 +4,7 @@ from pprint import pprint
 import creds
 from html import unescape
 import random
+# import sample_data
 
 # Global Variables
 QUESTIONS = []
@@ -56,6 +57,7 @@ def display_questions():
     answers below and take the users answer to be checked
     """
     print('printing questions...\n')
+    x = 0
     for question in QUESTIONS:
         this_question = question['question'] 
         print(unescape(this_question), '\n')
@@ -64,18 +66,60 @@ def display_questions():
         # print('correct answer is type: ', type(correct_answer), correct_answer)
         # print('incorrect answers is type: ',type(incorrect_answers), incorrect_answers, '\n')
         answers = incorrect_answers + [correct_answer]
-        random.shuffle(answers)
+        
+        # shuffle answers if more than two options
+        # otherwise sort into reverse alphabetical order
+        # so that the True/False order is consistent
+        if len(answers) > 2:
+            random.shuffle(answers)
+        else:
+            answers.sort(reverse = True)
+
         # print answers with number indexing
         answer_number = 1
         for answer in answers:
+            # remove unwanted spaces
+            answer.strip()
             print(unescape(f'No.{answer_number} {answer}'))
             answer_number += 1
         print('\n')
-        user_answer = input('Your answer: \n')
-   
+        while True:
+            user_answer = input('Your answer: \n')
+            if validate_input(user_answer, x):
+                break
+                # check_answer(user_answer)
+        x += 1
+
+# def check_answer(answer):
+    # print('checking answer...')
+
+def validate_input(ans, index):
+    """
+    Validate user input to be a number 1-4 or
+    1-2 if answer is boolean.
+    """
+    print('validating user input...')
+    ans_type = QUESTIONS[index]['type']
+    ans_range = 4
+    if ans_type == 'multiple':
+        pass
+    else:
+        ans_range = 2
+    
+    try:
+        ans = int(ans)
+        if not (1 <= ans <= ans_range):
+            print('if working.....')
+            raise ValueError(
+                f"Expected an input of '1 - {ans_range}', but got '{ans}'. Please try again."
+            )    
+    except ValueError as e:
+        print(f"Invalid data: {e}, please try again.\n")
+        return False
 
 
+    return True
 
 
-get_questions(2, 9, DIFFICULTY)
+get_questions(10, 9, DIFFICULTY)
 display_questions()
