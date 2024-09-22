8560ebe2cd69f00cc24db5d432ec1619c14ec6c3
David O'Quigley <oquigld2@gmail.com>
2024-09-22 15:02:42 +0000

    test push

--- run.py
+++ run.py
@@ -3,17 +3,23 @@ import requests
 from pprint import pprint
 import creds
 from html import unescape
+import random
 
 # Global Variables
-QUESTIONS = {}
+QUESTIONS = []
+USERNAME = ''
+SCORE = 0
+DIFFICULTY = 'easy'
 
 
 
-def getQuestion(amount, category, difficulty):
+def get_questions(amount, category, difficulty):
     """
     Function to get a set of questions from the Trivia Questions API.
     It stores the the list of 10 Q's in a variable to be called later
     """
+    print('fetching questions...\n')
+
     url = f"https://trivia-questions-api.p.rapidapi.com/triviaApi?amount={amount}&category={category}&difficulty={difficulty}"
 
     headers = {
@@ -23,15 +29,53 @@ def getQuestion(amount, category, difficulty):
 
     response = requests.get(url, headers=headers)
     # convert response object to python dict
+    global QUESTIONS
     QUESTIONS = response.json()
-    pprint(QUESTIONS, depth=5)
-    print('\n')
-    print('\n')
-    i = 1
-    this_question = QUESTIONS['triviaQuestions'][i]['question']
-    # print(unescape(QUESTIONS['triviaQuestions'][0]['question']))
-    print(unescape(this_question))
-    correct_answer = QUESTIONS['triviaQuestions'][i]['correct_answer']
-    print(unescape(f'The correct answer is: {correct_answer}'))
+    QUESTIONS = QUESTIONS['triviaQuestions']
+    # print('QUESTIONS type is:', type(QUESTIONS))
+    # print('QUESTIONS length is:', len(QUESTIONS))
 
-getQuestion(2, 9, 'easy')
+
+    # pprint(QUESTIONS, depth=5)
+    i = 1
+    this_question = QUESTIONS[i]['question']
+    correct_answer = QUESTIONS[i]['correct_answer']
+    incorrect_answers_list = QUESTIONS[i]['incorrect_answers']
+    
+    # print('number of questions: ', len(QUESTIONS))
+    # print(unescape(this_question))
+    # print(unescape(f'The correct answer is: {correct_answer}'))
+    # print(unescape(f'The wrong answers are: {incorrect_answers_list}'))
+    # return a string with 1)The question 2)the correct answer and 3)the incorrect answers
+    # return [this_question, correct_answer]
+
+
+def display_questions():
+    """
+    Display the questions, one at a time with the multiply choice
+    answers below and take the users answer to be checked
+    """
+    print('printing questions...\n')
+    for question in QUESTIONS:
+        this_question = question['question'] 
+        print(unescape(this_question), '\n')
+        incorrect_answers = question['incorrect_answers']
+        correct_answer = question['correct_answer']
+        # print('correct answer is type: ', type(correct_answer), correct_answer)
+        # print('incorrect answers is type: ',type(incorrect_answers), incorrect_answers, '\n')
+        answers = incorrect_answers + [correct_answer]
+        random.shuffle(answers)
+        # print answers with number indexing
+        answer_number = 1
+        for answer in answers:
+            print(unescape(f'No.{answer_number} {answer}'))
+            answer_number += 1
+        print('\n')
+        user_answer = input('Your answer: \n')
+   
+
+
+
+
+get_questions(2, 9, DIFFICULTY)
+display_questions()
