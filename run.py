bef48fe4e1173b51110b0ff07ae838b60b17d7bf
David O'Quigley <oquigld2@gmail.com>
2024-09-23 16:32:49 +0000

    Add menu selction to end of round, squash bugs

--- run.py
+++ run.py
@@ -16,7 +16,8 @@ from simple_term_menu import TerminalMenu
 QUESTIONS = []
 USERNAME = ''
 SCORE = 0
-DIFFICULTY = 'easy'
+CATEGORY = 12
+DIFFICULTY = 'hard'
 
 
 def clr_terminal():
@@ -30,10 +31,10 @@ def get_questions(amount, category, difficulty):
     Function to get a set of questions from the Trivia Questions API.
     It stores the the list of 10 Q's in a variable to be called later
     """
-    print('Loading questions...\n')
+    print(f'Loading {amount} questions...\n')
+    time.sleep(1)
 
-    url = f"""https://trivia-questions-api.p.rapidapi.com/triviaApi?
-    amount={amount}&category={category}&difficulty={difficulty}"""
+    url = f"""https://trivia-questions-api.p.rapidapi.com/triviaApi?amount={amount}&category={category}&difficulty={difficulty}"""
 
     headers = {
         "x-rapidapi-key": creds.api_key,
@@ -45,13 +46,14 @@ def get_questions(amount, category, difficulty):
     global QUESTIONS
     QUESTIONS = response.json()
     QUESTIONS = QUESTIONS['triviaQuestions']
-    # print('QUESTIONS type is:', type(QUESTIONS))
-    # print('QUESTIONS length is:', len(QUESTIONS))
-    # pprint(QUESTIONS, depth=5)
-    i = 1
-    this_question = QUESTIONS[i]['question']
-    correct_answer = QUESTIONS[i]['correct_answer']
-    incorrect_answers_list = QUESTIONS[i]['incorrect_answers']
+    print('amount type is:', type(amount))
+    print('QUESTIONS type is:', type(QUESTIONS))
+    print('QUESTIONS length is:', len(QUESTIONS))
+    pprint(QUESTIONS, depth=5)
+    # i = 1
+    # this_question = QUESTIONS[i]['question']
+    # correct_answer = QUESTIONS[i]['correct_answer']
+    # incorrect_answers_list = QUESTIONS[i]['incorrect_answers']
     # print('number of questions: ', len(QUESTIONS))
     # print(unescape(this_question))
     # print(unescape(f'The correct answer is: {correct_answer}'))
@@ -71,12 +73,22 @@ def display_questions():
 
         this_question = question['question']
         rprint(f'[bold bright_cyan]Qusetion {x+1}: ')
-        print(textwrap.dedent(unescape(this_question)), '\n')
-
+        print(textwrap.fill(textwrap.dedent(unescape(this_question))), '\n')
+        
         incorrect_answers = question['incorrect_answers']
         correct_answer = question['correct_answer']
         answers = incorrect_answers + [correct_answer]
-        print(f'cheat ans is: {correct_answer}')
+
+        print(correct_answer)
+
+        correct_answer = format_data(correct_answer)
+        answers = format_data(answers)
+        # remove white any white space from answers and
+        # unescape special characters
+        # for answer in answers:
+        #     answer_new = unescape(answer.strip())
+        #     answers[answers.index(answer)] = answer_new
+        # print(f'cheat ans is: {correct_answer}')
 
         # shuffle answers if more than two options
         # otherwise sort into reverse alphabetical order
@@ -87,39 +99,64 @@ def display_questions():
             answers.sort(reverse=True)
 
         time.sleep(1.5)
-        display_answer_options(answers)
-        TEMPANSWER = answers.index(correct_answer) + 1 #delete this
+        user_answer = display_answer_options(answers)
+        check_answer(user_answer, correct_answer)
+        #delete this
+        # TEMPANSWER = answers.index(correct_answer) + 1
 
-        while True:
-            # user_answer = input('Your answer: \n')
-            user_answer = TEMPANSWER # delete this
-            if validate_answer(user_answer, x):
-                check_answer(user_answer, correct_answer, answers)
-                break
-
+        # while True:
+        #     user_answer = input('Your answer: \n')
+        #     # delete this
+        #     # user_answer = TEMPANSWER
+        #     if validate_answer(user_answer, x):
+        #         check_answer(user_answer, correct_answer, answers)
+        #         break
+        
         x += 1
-    game_over()
+    round_over()
+
+
+def format_data(data):
+    """
+    Function to strip leading and trailing white space
+    As well as unescaping special characters
+    """
+    if type(data) == str:
+        data = unescape(data.strip())
+        return data
+    for x in data:
+        new_x = unescape(x.strip())
+        data[data.index(x)] = new_x
+    return data
+
 
 
 def display_answer_options(answers):
     """
     Print answers with numbered indexing.
     """
-    answer_number = 1
-    for answer in answers:
-        # remove unwanted spaces
-        answer = answer.strip()
-        print(unescape(f'No.{answer_number} {answer}'))
-        answer_number += 1
-        time.sleep(.2)
+    answer_menu = TerminalMenu(answers)
+    answer_selection = answer_menu.show()
+    print(f'you have selected {answers[answer_selection]}')
+    return answers[answer_selection]
+
+    # answer_number = 1
+    # for answer in answers:
+    #     # remove unwanted spaces
+    #     answer = answer.strip()
+    #     print(unescape(f'No.{answer_number} {answer}'))
+    #     answer_number += 1
+    #     time.sleep(.2)
     print('\n')
 
 
-def check_answer(input_ans, correct_ans, ans_list):
-    input_ans = int(input_ans)
+def check_answer(input_ans, correct_ans):
+    """
+    Function to compare selected ans with actual
+    correct answer
+    """
     global SCORE
-    ans_index = ans_list.index(correct_ans) + 1
-    if input_ans == ans_index:
+    if input_ans == correct_ans:
         rprint('[bold green]Correct!\n')
         SCORE += 1
     else:
@@ -128,7 +165,7 @@ def check_answer(input_ans, correct_ans, ans_list):
     clr_terminal()
 
 
-def game_over():
+def round_over():
     """
     Function to display score and further options;
     play again view leader board.
@@ -146,15 +183,41 @@ def game_over():
         case SCORE if SCORE < 9:
             rprint(f'Great job, you scored [bold purple]{SCORE}[/bold purple] out of 10.')
         case SCORE if SCORE < 10:
-            rprint(textwrap.dedent(f'''
+            rprint(textwrap.fill(textwrap.dedent(f'''
             You got [bold purple]{SCORE}[/bold purple] questions
-             right, nearly a perfect score. Well done!'''))
+             right, nearly a perfect score. Well done!\n''')))
         case 10:
-            rprint(textwrap.dedent('''
+            rprint(textwrap.fill(textwrap.dedent('''
             CONGRATULATIONS! You scored a perfect round
-            of [bold purple]10/10.[/bold purple]Well done.'''))
+            of [bold purple]10/10.[/bold purple] Well done.\n''')))
     # check leader board
-    print('game over\n')
+
+    time.sleep(2)
+    clr_terminal()
+
+    print('What would you like to do?\n')
+    replay = [
+        'Replay this round',
+        'Try another category',
+        'View leader board',
+        'Exit'
+        ]
+    user_input = TerminalMenu(replay)
+    input_index = user_input.show()
+    action = replay[input_index]
+    # replay = [
+    #     'entry 1',
+    #     'entry 2',
+    #     '',
+    #     'entry 3'
+    #     ]
+    # terminal_menu = TerminalMenu(replay)
+    # menu_entry_index = terminal_menu.show()
+    # print(f"You have selected {replay[menu_entry_index]}!")
+
+    print(action)
+
+    
 
 
 def validate_answer(ans, index):
@@ -194,10 +257,11 @@ def start_up():
     and difficulties.\n'''))
     time.sleep(1.5)
     (category, cat_index) = category_select()
-    rprint(f'You have selected [bold purple]{category}.')
+    # rprint(f'You have selected [bold purple]{category}.')
     time.sleep(1.5)
     difficulty = difficulty_select()
-    rprint(f'You have selected [bold purple]{difficulty}.')
+    # rprint(f'You have selected [bold purple]{difficulty}.')
+    difficulty = difficulty.lower()
     time.sleep(1.5)
     category = convert_selection(cat_index)
 
@@ -208,10 +272,11 @@ def difficulty_select():
     """
     Do stuff
     """
-    diff_options = ['easy', 'medium', 'hard']
+    diff_options = ['Easy', 'Medium', 'Hard']
     print('Please choose a difficulty level from the list below:\n')
     difficulty_menu = TerminalMenu(diff_options)
     diff_selection = difficulty_menu.show()
+    rprint(f'You have selected [bold purple]{diff_options[diff_selection]}')
     return diff_options[diff_selection]
 
 
@@ -226,18 +291,16 @@ def category_select():
     print('Please choose a category of questions from the list below:\n')
     category_menu = TerminalMenu(cat_options)
     cat_selection = category_menu.show()
-    # print(f'You have selected {cat_options[menu_selection]}')
+    rprint(f'You have selected [bold purple]{cat_options[cat_selection]}')
     return (cat_options[cat_selection], cat_selection)
 
 
 def convert_selection(selection):
     """
-    Do stuff
+    Convert menu indices into API readable values
     """
     api_categories = [9, 10, 11, 12, 14, 21, 22, 23]
     return api_categories[selection]
-
-
 
 
 def validate_input(category, difficulty):
@@ -247,13 +310,19 @@ def validate_input(category, difficulty):
     pass
 
 
+
 def main():
     """
     List of functions to run on programme launch
     """
-    (a, b) = start_up()
-    get_questions(10, a, b)
+    global CATEGORY
+    global DIFFICULTY
+    (CATEGORY, DIFFICULTY) = start_up()
+    get_questions(1, CATEGORY, DIFFICULTY)
     display_questions()
 
 
 main()
+
+
+
